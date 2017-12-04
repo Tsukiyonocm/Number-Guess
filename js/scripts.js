1d68eefdbd8f09913ee021b279714d8bc456778d
Tsukiyonocm <tsukiyonocm@yahoo.com>
2017-12-04 10:04:44 -0500

    added initial files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var getGuess = prompt("Pick a number between 1 and 5.");
+var number = 3
+
+if (getGuess === number){
+    console.log("You guessed correctly!");
+}
+else {
+    console.log("You guessed poorly.");
+}
