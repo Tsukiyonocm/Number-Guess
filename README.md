de4da4d1da2c28fbc28a37a3e033200b23eaea5a
Tsukiyonocm <tsukiyonocm@yahoo.com>
2017-12-11 22:44:04 -0500

    added in basic dom functionality

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-"# Number-Guess" 
+"# Number-Guess"
+
+//Step 1
+    1. Setup a default variable for the guess. (this will likely be blank for
+    awhile).
+    2. Setup a variable to hold the value for the number to be guessed.
+
+
+//Step 2
+    1. Create a simple function in order to compare the selected Number
+    to the guessed number.
+    2. Create a function in order to generate a number between any two sets of
+    numbers.
+
+//Step 3
+    1. Create an html input for the guessed number.
+    2. Add wrong guesses to an array.
+    3. 
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,45 @@
 var magicNumber = 0;
 var missedGuesses = 0;
 
-//Compares magicNumber to the guessed number.
-function compareGuess (num){
-    if (num === magicNumber){
-        console.log("correct");
-        console.log("You guessed wrong: " + missedGuesses + " times.");
-    }
-    else {
-        console.log("wrong");
-        missedGuesses++;
+var guessingGame = {
+
+    //compares magicNumber to Guessed Number
+    playerGuess: function(num){
+        if (num === magicNumber){
+            console.log("correct");
+            console.log("You guessed wrong: " + missedGuesses + " times.");
+        }
+        else {
+            console.log("wrong");
+            missedGuesses++;
+        }
+    },
+
+    //Creates a random guess between two values stored as magicNumber
+    randomNumber: function(max, min){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        magicNumber = Math.floor(Math.random()* (max - min + 1) * min);
+        console.log(magicNumber);
     }
 }
 
-//Generates a randomNumber stored in magicNumber.
-function randomNumber (max, min){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    magicNumber = Math.floor(Math.random()* (max - min + 1) * min);
-    console.log(magicNumber);
+
+var handlers = {
+    
+    guessButton: function(){
+        //Pulls Guessed Number from Input
+        var guessNum = document.getElementById("guess");
+        guessingGame.playerGuess(guess.valueAsNumber);
+        guess.value = "";
+    },
+
+    randomGenButton: function(){
+        //Sets Max and Min Values for Guessing Game
+        var maxNumber = document.getElementById("maxNumber");
+        var minNumber = document.getElementById("minNumber");
+        guessingGame.randomNumber(maxNumber.valueAsNumber, minNumber.valueAsNumber);
+        maxNumber.value = "";
+        minNumber.value = "";
+    }
 }
